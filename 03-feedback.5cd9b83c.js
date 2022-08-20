389580ed7cc20bf62f2b209f001d70015cc1c2e7
Vetali1985 <100028292+Vetali1985@users.noreply.github.com>
2022-08-20 18:01:23 +0000

    Deploying to gh-pages from @ Vetali1985/goit-js-hw-08@887a69da9547108f6103061acbfdf3952891dcd3 🚀

--- 03-feedback.5cd9b83c.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={form:document.querySelector(".feedback-form"),textarea:document.querySelector(".feedback-form textarea")};e.form.addEventListener("submit",(function(e){e.preventDefault(),console.log(e.target.value)})),e.textarea.addEventListener("input",(function(e){var t=e.currentTarget.value;console.log,localStorage.setItem("feedback-form-state",t)}))}();
-//# sourceMappingURL=03-feedback.5cd9b83c.js.map
--- 03-feedback.8cccfcc5.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector(".feedback-form"),textarea:document.querySelector(".feedback-form textarea")};e.form.addEventListener("submit",(function(e){e.preventDefault(),console.log(e.target.value)})),e.textarea.addEventListener("input",(function(e){const t=e.currentTarget.value;console.log,localStorage.setItem("feedback-form-state",t)}));
-//# sourceMappingURL=03-feedback.8cccfcc5.js.map
