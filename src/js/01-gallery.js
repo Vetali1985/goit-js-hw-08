f26811fcd6917c46bad895c3b5bf2b0da32bc997
Vetali1985 <geelycross1985@gmail.com>
2022-08-20 19:52:36 +0300

    12

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const imageEl = document.querySelector(`.gallery`)
+const imageMarkup = createGalleryItems(galleryItems)
+imageEl.insertAdjacentHTML(`beforeend`, imageMarkup)
+
+
+function createGalleryItems(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return`
+            <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+    })
+        .join(" ");
+}
+
+    new SimpleLightbox('.gallery a', {
+        captionPosition: 'bottom',
+         captionDelay: 250 ,
+    });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,14 @@
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea:document.querySelector('.feedback-form textarea')
+}
+refs.form.addEventListener('submit', onSubmit);
+refs.textarea.addEventListener('input', onInput);
+function onSubmit(evt) {
+   console.log()
+}
+function onInput(evt) {
+    const message = evt.currentTarget.value;
+   localStorage.setItem('feedback', message)
+}
