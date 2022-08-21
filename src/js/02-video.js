8decfc2aa6c81d91901c34d5c4bae2acac21ff36
Vetali1985 <geelycross1985@gmail.com>
2022-08-21 15:11:35 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,18 +2,27 @@
 import Player from '@vimeo/player';
 const throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
+const player = new Player(iframe);
 const STORAGE_KEY = "videoplayer-current-time"
+
     player.on('timeupdate', throttle(timeUpdate,1000));
 
 function timeUpdate(data) {
-     evt.preventDefault();
 localStorage.setItem(STORAGE_KEY, JSON.stringify(data))
-
-    console.log(data);
-
 }
-// function setCurrentTime(data) {
-//     evt.preventDefault();
+    const timePause = JSON.parse(localStorage.getItem(STORAGE_KEY)).seconds;
+
+player.setCurrentTime(timePause).then(function (seconds) {
     
-//    }
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
